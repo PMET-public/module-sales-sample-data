aad7d6b0ae17f467e6f317ddf7930466330cc88d
Jeff Britts <jbritts@magento.com>
2016-09-22 14:09:39 -0400

    initial commit

---
+++ Model/Order.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace MagentoEse\SalesSampleData\Model;
+
+use Magento\Framework\Setup\SampleData\Context as SampleDataContext;
+
+/**
+ * Class Order
+ */
+class Order
+{
+    /**
+     * @var \Magento\Framework\File\Csv
+     */
+    protected $csvReader;
+
+    /**
+     * @var \Magento\Framework\Setup\SampleData\FixtureManager
+     */
+    protected $fixtureManager;
+
+    /**
+     * @var Order\Converter
+     */
+    protected $converter;
+
+    /**
+     * @var Order\Processor
+     */
+    protected $orderProcessor;
+
+    /**
+     * @var \Magento\Sales\Model\ResourceModel\Order\CollectionFactory
+     */
+    protected $orderCollectionFactory;
+
+    /**
+     * @var \Magento\Customer\Api\CustomerRepositoryInterface
+     */
+    protected $customerRepository;
+
+    /**
+     * @param SampleDataContext $sampleDataContext
+     * @param Order\Converter $converter
+     * @param Order\Processor $orderProcessor
+     * @param \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory
+     * @param \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository
+     */
+    public function __construct(
+        SampleDataContext $sampleDataContext,
+        Order\Converter $converter,
+        Order\Processor $orderProcessor,
+        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $orderCollectionFactory,
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        \Magento\Sales\Model\OrderFactory $orderFactory
+    ) {
+        $this->fixtureManager = $sampleDataContext->getFixtureManager();
+        $this->csvReader = $sampleDataContext->getCsvReader();
+        $this->converter = $converter;
+        $this->orderProcessor = $orderProcessor;
+        $this->orderCollectionFactory = $orderCollectionFactory;
+        $this->customerRepository = $customerRepository;
+        $this->orderFactory = $orderFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function install(array $fixtures)
+    {
+        //$t = $this->orderFactory->create();
+        //$t->load(1);
+
+
+
+        foreach ($fixtures as $file) {
+            $fileName = $this->fixtureManager->getFixture($file);
+            if (!file_exists($fileName)) {
+                continue;
+            }
+
+            $rows = $this->csvReader->getData($fileName);
+            $header = array_shift($rows);
+
+            $isFirst = true;
+            foreach ($rows as $row) {
+                $data = [];
+                foreach ($row as $key => $value) {
+                    $data[$header[$key]] = $value;
+                }
+                $row = $data;
+                /*if ($isFirst) {
+                    $customer = $this->customerRepository->get($row['customer_email']);
+                    if (!$customer->getId()) {
+                        continue;
+                    }
+
+                    $orderCollection = $this->orderCollectionFactory->create();
+                    $orderCollection->addFilter('customer_id', $customer->getId());
+                    if ($orderCollection->count() > 0) {
+                        break;
+                    }
+                }
+                $isFirst = false;*/
+                $orderData = $this->converter->convertRow($row);
+                $this->orderProcessor->createOrder($orderData);
+            }
+        }
+    }
+}
